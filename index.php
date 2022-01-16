3f3834c4b2f8b0d6e27a5eb6dc31a021a617a324
Bernardo Knoblauch <beknoblauch18@gmail.com>
2022-01-16 20:55:09 -0300

    new: final module project

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+    //Declarar variaveis
+    $servername = "localhost";
+    $user = "root";
+    $pass = "123654be";
+    //Conectar tratando excessoes
+    try {
+        $conn = new PDO("mysql:host=$servername;dbname=test", $user, $pass);
+        $conn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        echo "Connection successfull";
+    } catch(PDOException $e) {
+        echo "Connection failed  " . $e->getMessage();
+    }
+
+    $conn = null;
+    //Criar Database
+    //Criar Table
+    //Pedir entrada do usuário
+    //Tratar input do prompt
+    //Executar operação desejada
+    //
+?>
