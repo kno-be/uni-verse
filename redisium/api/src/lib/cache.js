284c1ca08f6ea58a10ab341e3d2a14223842210f
berhart <beknoblauch18@gmail.com>
2022-02-15 19:23:58 -0300

    commit on main

---
+++ redisium/api/src/lib/cache.js
@@ -0,0 +1,30 @@
+const Redis = require('ioredis')
+
+class Cache {
+    constructor() {
+        this.redis = new Redis({
+            host: process.env.REDIS_HOST || "localhost",
+            port: process.env.REDIS_PORT || 6379,
+            keyPrefix: "cache:"
+        })
+    }
+    async get(key) {
+        const value = await this.redis.get(key)
+        return value ? JSON.parse(value): null;
+    }
+
+    set(key, value) {
+        return this.redis.set(key, JSON.stringify(value))
+    }
+    del(key){
+        return this.redis.del(key)
+    }
+    async delPrefix(prefix){
+        const keys = await this.redis.keys(`cache:${prefix}:*`)).map((key) => 
+        key.replace("cache:", ""))
+
+        return this.redis.del(keys)
+    }
+}
+
+module.exports = new caches()
